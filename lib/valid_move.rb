52aebb7801e9c25e20be06d2b237edcaa2e9201f
Harley Hanna <harleyhannawrit@gmail.com>
2020-05-31 21:08:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil || board[index] < 0 || board[index] >
+  if board[index] == " " || board[index] == "" || board[index] == nil || board[index] < 0 || board[index] > 8
     return false
   elsif board[index] == "X" || board[index] == "O" || board[index].between?(0,8)
     return true
