a18fd473017e25bd1476adb68267e735c98dbe7f
Harley Hanna <harleyhannawrit@gmail.com>
2020-05-31 21:12:07 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move? (board, index)
-  if board[index] > 8 || board[index] < 0
+  if board[index] >= 9 || board[index] <= -1
     return false
   elsif position_taken?(board, index) == true
     return false
